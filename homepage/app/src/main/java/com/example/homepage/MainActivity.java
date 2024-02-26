27442cf0c38d6fad6895e68e5de8dbda873ba325
Avani-Khairnar <110759110+Avani-Khairnar@users.noreply.github.com>
2024-02-26 16:22:21 +0530

    Add files via upload

---
+++ homepage/app/src/main/java/com/example/homepage/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    CardView clothingCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        clothingCard=findViewById(R.id.clothingCard);
+        clothingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this, Clothingactivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
